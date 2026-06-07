13c325c0669167c21d24b5f83e5d9854db6c72c9
weisleydan49 <weisleydan49@gmail.com>
2026-06-07 04:01:45 -0700

    added a sum of indices in arras

--- Java/ku.1.2/OOP/tests/Hospital.java
+++ Java/ku.1.2/OOP/tests/Hospital.java
@@ -1,76 +1,50 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
-public class Hospital extends JFrame {
+//If temp is above 38 and pressure is above 140: admit patient
+//if pressure is below 140, give medication
+//if temp is below 38 and pressure is above 140: send to the lab
+/*import javax.swing.*;
 
-    // Fields
-    private JTextField tempField;
-    private JTextField pressureField;
-    private JTextField conclusionField;
+public class Hospital {
+    public static void main(String[] args) {
+        int temp = Integer.parseInt(JOptionPane.showInputDialog("Enter the temperature: "));
+        String prs = JOptionPane.showInputDialog("Enter the pressure: ");
+        int pressure = Integer.parseInt(prs);
 
-    // Main GUI
-    public Hospital() {
-        setTitle("ALMOND HOSPITAL");
-        setSize(1200, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3, 2, 20, 20));
-
-        // Font
-        Font font = new Font("Arial", Font.PLAIN, 30);
-
-        // Temperature
-        JLabel tmp = new JLabel("Enter temperature");
-        tmp.setFont(font);
-        tempField = new JTextField();
-        add(tmp);
-        tempField.setFont(font);
-        add(tempField);
-
-        // Pressure field
-        JLabel pressure = new JLabel("Enter pressure");
-        pressure.setFont(font);
-        pressureField = new JTextField();
-        add(pressure);
-        pressureField.setFont(font);
-        add(pressureField);
-
-        JButton conclusionBtn = new JButton("Diagnosis");
-        conclusionBtn.setFont(font);
-        conclusionBtn.addActionListener(new AddListener());
-        add(conclusionBtn);
-
-        conclusionField = new JTextField();
-        conclusionField.setFont(font);
-        conclusionField.setEditable(false);
-        add(conclusionField);
-
-        setVisible(true); // makes the window actually appear
-    }
-
-    private class AddListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            try {
-                // Get the values of temperature and pressure
-                double t = Double.parseDouble(tempField.getText());
-                double p = Double.parseDouble(pressureField.getText());
-
-                // Analyse and give a conclusion
-                if (t > 38 && p > 140) {
-                    conclusionField.setText(String.valueOf("Admit"));
-                } else if (t < 38 && p < 140) {
-                    conclusionField.setText(String.valueOf("Give Medication"));
-                } else if (p < 140) {
-                    conclusionField.setText(String.valueOf("Send to the lab"));
-                }
-            } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(null, "Enter valid values");
-            }
-
+        if (temp > 38 && pressure > 140) {
+            JOptionPane.showMessageDialog(null, "Admit the patient");
+        } else if (pressure < 140) {
+            JOptionPane.showMessageDialog(null, "Give medication");
+        } else if (temp < 38 && pressure > 140) {
+            JOptionPane.showMessageDialog(null, "Send to the lab");
+        } else {
+            JOptionPane.showMessageDialog(null, "Enter a valid integer");
         }
     }
+}
 
+*/
+import java.util.Scanner;
+
+public class Hospital {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Hospital());
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+
+        System.out.println("Enter temperature:");
+        int temp = sc.nextInt();
+
+        System.out.println("Enter pressure: ");
+        int pressure = sc1.nextInt();
+
+        if (temp > 38 && pressure > 140) {
+            System.out.println("Admit the patient");
+        } else if (pressure < 140) {
+            System.out.println("Give medication");
+        } else if (temp < 38 && pressure > 140) {
+            System.out.println("Send to the lab");
+        } else {
+            System.out.println("Enter a valid integer");
+        }
+
     }
 }
